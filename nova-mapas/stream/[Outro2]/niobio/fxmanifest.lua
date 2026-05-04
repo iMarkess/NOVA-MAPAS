557253b5133d05a737048b137dfa8c7f9a948c17
iMarkess <100230589+iMarkess@users.noreply.github.com>
2026-05-04 19:45:35 -0300

    att nova-mapas

---
+++ nova-mapas/stream/[Outro2]/niobio/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Aurora DEV#0726'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+files {
+    'stream/*.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/*.ytyp'
