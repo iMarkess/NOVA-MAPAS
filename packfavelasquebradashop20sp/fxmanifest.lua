1daf29b396548d3178ba3305fd659a75a6ada30b
iMarkess <100230589+iMarkess@users.noreply.github.com>
2026-05-04 19:13:32 -0300

    add favelas

---
+++ packfavelasquebradashop20sp/fxmanifest.lua
@@ -0,0 +1,24 @@
+shared_script '@eqpg-pro/events.lua'
+client_script '@eqpg-pro/client_shared.lua'
+server_script '@eqpg-pro/server_shared.lua'
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Quebrada SHOP discord.gg/eVCFYSgJxy'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/**/*.ytd' 
+}
+
+
+
+dependency '/assetpacks'
+
+--DECRYPTED & 3D FIXED BY CHEAPM. discord.gg/Sac9mJ7SAb     https://cheapmfxap.com/
+--DECRYPTED & 3D FIXED BY CHEAPM. discord.gg/Sac9mJ7SAb     https://cheapmfxap.com/
+--DECRYPTED & 3D FIXED BY CHEAPM. discord.gg/Sac9mJ7SAb     https://cheapmfxap.com/
+--DECRYPTED & 3D FIXED BY CHEAPM. discord.gg/Sac9mJ7SAb     https://cheapmfxap.com/
+--DECRYPTED & 3D FIXED BY CHEAPM. discord.gg/Sac9mJ7SAb     https://cheapmfxap.com/
