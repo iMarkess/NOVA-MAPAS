9859bab28f8cee4d879b86741a37459b2fe533b3
iMarkess <100230589+iMarkess@users.noreply.github.com>
2026-05-06 20:46:00 -0300

    general cleaning, maps removed

--- cfx-casino/__resource.lua
+++ cfx-casino/__resource.lua
@@ -1,5 +1,8 @@
+shared_script '@eqpg-pro/events.lua'
+client_script '@eqpg-pro/client_shared.lua'
+server_script '@eqpg-pro/server_shared.lua'
 
 
 this_is_a_map "yes"
 resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
-client_script 'client.lua'              
+client_script 'client.lua'
--- pablito_lamesa_prison/fxmanifest.lua
+++ pablito_lamesa_prison/fxmanifest.lua
@@ -1,3 +1,6 @@
+shared_script '@eqpg-pro/events.lua'
+client_script '@eqpg-pro/client_shared.lua'
+server_script '@eqpg-pro/server_shared.lua'
 fx_version 'cerulean'
 game 'gta5'
 
