5c26439badaab2d13e2b982852cb9a2d263c6b7d
iMarkess <100230589+iMarkess@users.noreply.github.com>
2026-05-05 23:02:35 -0300

    add hospital and remove useless files

---
+++ Babwaa_Studio_The_Richman_Hospital/__resource.lua
@@ -0,0 +1,7 @@
+shared_script '@eqpg-pro/events.lua'
+client_script '@eqpg-pro/client_shared.lua'
+server_script '@eqpg-pro/server_shared.lua'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+this_is_a_map 'yes'
