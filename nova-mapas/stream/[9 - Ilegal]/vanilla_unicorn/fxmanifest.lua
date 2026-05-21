ddebdf8a68f316cc4b161d2f7ebaede4369d3da5
iMarkess <100230589+iMarkess@users.noreply.github.com>
2026-05-20 22:48:09 -0300

    mapas retirados

--- nova-mapas/stream/[9 - Ilegal]/vanilla_unicorn/fxmanifest.lua
+++ nova-mapas/stream/[9 - Ilegal]/vanilla_unicorn/fxmanifest.lua
@@ -1,6 +1,4 @@
-client_script "@vrp/lib/lib.lua" --Para remover esta pendencia de todos scripts, execute no console o comando "uninstall"
-
-fx_version "bodacious"
+fx_version "cerulean"
 game "gta5"
 
 this_is_a_map "yes"                                                                   
